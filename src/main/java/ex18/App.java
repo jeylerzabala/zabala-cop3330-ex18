5e0f9218b4b4ef442ec2dd5e67a20252329f8355
Jeyler desktop <jeylerzabala@gmail.com>
2021-06-06 14:01:47 -0400

    Not done yet

--- src/main/java/ex18/App.java
+++ src/main/java/ex18/App.java
@@ -1,6 +1,10 @@
 package ex18;
 
 /*
+
+*  UCF COP3330 Summer 2021 Assignment 1 Solution
+*  Copyright 2021 Jeyler Zabala
+
 Exercise 18 - Temperature Converter
 You’ll often need to determine which part of a program is run based on user input or other events.
 
@@ -40,7 +44,41 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
+        int fahrenheit, celsius, fahrenheitToCelsius, celsiusToFahrenheit;
+
         Scanner in = new Scanner(System.in);
         System.out.print("Press C to convert from Fahrenheit to Celsius.\nPress F to convert from Celsius to Fahrenheit.\nYour choice: ");
         String tempScale = in.nextLine();
+
+        Scanner sc = new Scanner(System.in);
+
+        // C = (F − 32) × 5 / 9
+
+        fahrenheitToCelsius = (fahrenheit - 32) * 5 / 9;
+
+        // F = (C × 9 / 5) + 32
+
+        celsiusToFahrenheit = (celsius * 9 / 5) + 32;
+
+        if(tempScale.equals("C")){
+            System.out.print("Please enter the temperature in Fahrenheit: ");
+            fahrenheit = sc.nextInt();
+            System.out.println("The temperature in Celsius is " + fahrenheitToCelsius + ".");
+        }
+        else if(tempScale.equals("c")){
+            System.out.print("Please enter the temperature in Fahrenheit: ");
+            fahrenheit = sc.nextInt();
+            System.out.println("The temperature in Celsius is " + fahrenheitToCelsius + ".");
+        }
+        if(tempScale.equals("F")){
+            System.out.print("Please enter the temperature in Celsius: ");
+            celsius = sc.nextInt();
+            System.out.println("The temperature in Fahrenheit is " + celsiusToFahrenheit + ".");
+        }
+        else if(tempScale.equals("f")){
+            System.out.print("Please enter the temperature in Celsius: ");
+            celsius = sc.nextInt();
+            System.out.println("The temperature in Fahrenheit is " + celsiusToFahrenheit + ".");
+        }
+    }
 }
